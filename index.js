5bf4700823f1b3a0b0a19cba8e8f58fdaefc2a53
Noob <umutardakul@gmail.com>
2023-07-12 14:32:21 +0300

    deez nuts smasher

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var money = 0;
 var fuel = 0;
 var fuelGenerationPrice = 15;
+var requiredFuelForLaunch = 100;
+var LaunchedAmount = 0;
+var stone = 0;
 document.getElementById("Money").innerHTML = money;
 console.log("i love deez");
 
@@ -26,5 +29,15 @@ function startFuelGeneration(amountGenerating) {
     document.getElementById("Fuel").innerHTML = fuel;
     }, 1000);
   } 
+}
+
+function launchRocket(){
+    if (fuel >= requiredFuelForLaunch) {
+        fuel = 0;
+        money = 0;
+        fuelGenerationPrice = 0;
+        requiredFuelForLaunch = math.round(requiredFuelForLaunch^1.3)
+        LaunchedAmount = LaunchedAmount+1
+    }
 }
 
